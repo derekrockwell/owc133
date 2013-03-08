5244eb6312541a2faf1d008734da0ac19ac9a586
Derek <derek.a.rockwell@gmail.com>
2013-03-07 23:30:56 -0600

    add calendar migrations/seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,3 +20,6 @@ Refinery::Search::Engine.load_seed
 
 # Added by Refinery CMS News engine
 Refinery::News::Engine.load_seed
+
+# Added by Refinery CMS Venues extension
+Refinery::Calendar::Engine.load_seed
