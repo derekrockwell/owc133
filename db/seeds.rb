6f757b0131d4a965c83d6a97c657f3facab37bbb
Derek <derek.a.rockwell@gmail.com>
2013-03-07 23:28:32 -0600

    add inquiries migrations/seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,6 @@ Refinery::Pages::Engine.load_seed
 
 # Added by Refinery CMS Blog engine
 Refinery::Blog::Engine.load_seed
+
+# Added by Refinery CMS Inquiries engine
+Refinery::Inquiries::Engine.load_seed
