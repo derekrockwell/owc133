3764a6f8d2024d074d11658bb76c92f6471a6293
Derek <derek.a.rockwell@gmail.com>
2013-03-09 17:08:42 -0600

    changing model

--- vendor/extensions/htcs/app/models/refinery/htcs/volunteer.rb
+++ vendor/extensions/htcs/app/models/refinery/htcs/volunteer.rb
@@ -1,13 +1,15 @@
 module Refinery
-  module Htcs
-    class Volunteer < Refinery::Core::BaseModel
+	module Htcs
+		class Volunteer < Refinery::Core::BaseModel
 
-      attr_accessible :first_name, :last_name, :phone_number, :email, :address, :city, :state, :zip, :position
+			has_and_belongs_to_many :volunteer_categories
 
-      acts_as_indexed :fields => [:first_name]
+			attr_accessible :first_name, :last_name, :phone_number, :email, :address, :city, :state, :zip, :position
 
-      validates_presence_of :first_name, :last_name, :zip
-      validates :email, :presence => true, :uniqueness => true
-    end
-  end
+			acts_as_indexed :fields => [:first_name]
+
+			validates_presence_of :first_name, :last_name, :zip
+			validates :email, :presence => true, :uniqueness => true
+		end
+	end
 end
--- vendor/extensions/htcs/app/models/refinery/htcs/volunteer_category.rb
+++ vendor/extensions/htcs/app/models/refinery/htcs/volunteer_category.rb
@@ -2,6 +2,7 @@ module Refinery
   module Htcs
     class VolunteerCategory < Refinery::Core::BaseModel
 
+    	has_and_belongs_to_many :volunteer_categories
       attr_accessible :name, :sequence, :position
 
       acts_as_indexed :fields => [:name]
