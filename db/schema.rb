e2013d99254831b28d541f559f4ce670bc3c6789
Dave Collins <davecollins@cc-logic.com>
2013-03-10 01:50:02 -0600

    Isn't schema.rb super whiney?

--- db/schema.rb
+++ db/schema.rb
@@ -380,17 +380,17 @@ ActiveRecord::Schema.define(:version => 20130310020720) do
 
   add_index "refinery_users", ["id"], :name => "index_refinery_users_on_id"
 
-  create_table "roles_volunteers", :id => false, :force => true do |t|
-    t.integer "volunteer_id"
-    t.integer "role_id"
-  end
-  
   create_table "refinery_volunteer_categories", :force => true do |t|
     t.string   "name"
     t.integer  "sequence"
     t.integer  "position"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "roles_volunteers", :id => false, :force => true do |t|
+    t.integer "volunteer_id"
+    t.integer "role_id"
   end
 
   create_table "seo_meta", :force => true do |t|
