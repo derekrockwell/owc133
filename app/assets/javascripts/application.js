6077526f3b3a3d3de028d016d8561730c3ecd5c9
Derek <derek.a.rockwell@gmail.com>
2013-03-15 15:40:38 -0500

    styling first word

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,4 +24,13 @@ $(window).ready(function() {
         controlNav: true,
         manualControls: "#switches .carousel-switch"
     });
+
+
 });
+
+    $('.carousel-text h1, .carousel-switch h2, .story h2').each(function() {
+        var html = $(this).html();
+        var word = html.substr(0,html.indexOf(" "));
+        var rest = html.substr(html.indexOf(" "));
+        $(this).html(rest).prepend($("<span/>").html(word).addClass("first-word"));
+    });
